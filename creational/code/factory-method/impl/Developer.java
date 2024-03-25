080150bae671281fd96d6c23fa8c50ebc914829b
Abhijit Roy <161963891+abhijitxroy@users.noreply.github.com>
2024-03-25 11:40:18 +0530

    Create Developer.java

---
+++ creational/code/factory-method/impl/Developer.java
@@ -0,0 +1,10 @@
+package abstractfactorymethoddp.impl;
+
+import abstractfactorymethoddp.interf.Profession;
+
+public class Developer implements Profession{
+    
+    public void work(){
+        System.out.println("Profession: Developer");
+    }
+}
