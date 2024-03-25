da433815abd632c76cbc860ab2945c349b1c72c9
Abhijit Roy <161963891+abhijitxroy@users.noreply.github.com>
2024-03-25 11:40:34 +0530

    Add files via upload

---
+++ creational/code/factory-method/impl/DevOPs.java
@@ -0,0 +1,10 @@
+package abstractfactorymethoddp.impl;
+
+import abstractfactorymethoddp.interf.Profession;
+
+public class DevOPs implements Profession{
+    
+    public void work(){
+        System.out.println("Profession: DevOPs");
+    }
+}
