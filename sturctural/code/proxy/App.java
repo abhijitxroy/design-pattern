344bc36385c722701f35596c53e3efc4b8bf7274
Abhijit Roy <161963891+abhijitxroy@users.noreply.github.com>
2024-03-25 15:30:08 +0530

    Create App.java

---
+++ sturctural/code/proxy/App.java
@@ -0,0 +1,11 @@
+package designpattern;
+
+public class App {
+    
+    public static void main(String[] args) {
+       
+        Employee emp = new Proxy();
+        emp.getEmpDetails();
+    }
+    
+}
