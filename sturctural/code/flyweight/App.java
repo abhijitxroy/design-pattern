7ffe284d7e726198664a848538810ba3100bb56a
Abhijit Roy <161963891+abhijitxroy@users.noreply.github.com>
2024-03-25 23:51:01 +0530

    Create App.java

---
+++ sturctural/code/flyweight/App.java
@@ -0,0 +1,22 @@
+package designpattern;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Employee devEmployee1 = EmployeFacory.getDeveloper("Developer");
+        devEmployee1.getEmpDetails();
+
+        Employee devEmployee2 = EmployeFacory.getDeveloper("Developer");
+        devEmployee2.getEmpDetails();
+
+        Employee testerEmployee1 = EmployeFacory.getTester("Tester");
+        testerEmployee1.getEmpDetails();
+
+        System.out.println("devEmployee1 hashcode: " + devEmployee1.hashCode());
+        System.out.println("devEmployee2 hashcode: " + devEmployee2.hashCode());
+        System.out.println("testerEmployee1 hashcode: " + testerEmployee1.hashCode());
+
+    }
+
+}
