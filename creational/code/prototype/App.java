2fffb57b1c2d46f1515790425b5d0dab12334e2c
Abhijit Roy <161963891+abhijitxroy@users.noreply.github.com>
2024-03-25 12:46:24 +0530

    Update App.java

--- creational/code/prototype/App.java
+++ creational/code/prototype/App.java
@@ -1,1 +1,19 @@
+package abstractfactorymethoddp;
+
+public class App {
+    
+    public static void main(String[] args) {
+        ProfessionCache.loadProfessionCache();
+
+        Profession docProfession1 = ProfessionCache.getNewClProfession(1);
+        System.out.println(docProfession1);
+
+        Profession teacherProfession1 = ProfessionCache.getNewClProfession(2);
+        System.out.println(teacherProfession1);
+
+        Profession docProfession2 = ProfessionCache.getNewClProfession(1);
+        System.out.println(docProfession2);
+    }
+    
+}
 
