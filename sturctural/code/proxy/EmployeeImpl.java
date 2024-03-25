ec9a28dcb9995e80ba5be68f8012d851d97bfce3
Abhijit Roy <161963891+abhijitxroy@users.noreply.github.com>
2024-03-25 15:30:28 +0530

    Add files via upload

---
+++ sturctural/code/proxy/EmployeeImpl.java
@@ -0,0 +1,11 @@
+package designpattern;
+
+public class EmployeeImpl implements Employee {
+    
+    int id = 100;
+    String name = "ABhijit";
+
+    public void getEmpDetails(){
+        System.out.println("Emp Details: [ ID : " + this.id +  ", Name: "+ this.name +"]");
+    }
+}
---
+++ sturctural/code/proxy/Proxy.java
@@ -0,0 +1,29 @@
+package designpattern;
+
+import java.util.Scanner;
+
+public class Proxy extends EmployeeImpl {
+
+    private boolean isAuthenticate;
+
+    @SuppressWarnings("resource")
+    @Override
+    public void getEmpDetails() {
+        // First authenticate yourself then will fetch employee details if valid
+        System.out.println("Please authenticate yourself, will fetch employee details if valid");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Provide your auth Key: " + scan);
+
+        String authKey = scan.nextLine();
+        System.out.println("AuthKey is: " + authKey);
+
+        isAuthenticate = authKey.trim().equals("abhi123") ? true : false;
+
+        if (isAuthenticate) {
+            super.getEmpDetails();
+        } else {
+            System.out.println("You are failed to authenticate yourself.");
+        }
+
+    }
+}
