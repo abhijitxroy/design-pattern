705059bf0cb5d5bbffbb2d774cdaf26fe2622fb5
abhijitxroy <abhijit.x.roy@hotmail.com>
2025-07-19 20:25:19 +0530

    Commit for strategy design pattern

--- behavioral/strategy/StrategyDesignPattern.java
+++ behavioral/strategy/StrategyDesignPattern.java
@@ -2,58 +2,58 @@ package behavioral.strategy;
 
 // Client Code
 public class StrategyDesignPattern {
-	public static void main(String[] args) {
-		OnlineShoppingCart cart = new OnlineShoppingCart();
-		cart.setPaymentStrategy(new CashPaymentStrategy());
-		cart.checkout(1000);
+  public static void main(String[] args) {
+    OnlineShoppingCart cart = new OnlineShoppingCart();
+    cart.setPaymentStrategy(new CashPaymentStrategy());
+    cart.checkout(1000);
 
-		cart.setPaymentStrategy(new UpiPaymentStrategy());
-		cart.checkout(800);
-	}
+    cart.setPaymentStrategy(new UpiPaymentStrategy());
+    cart.checkout(800);
+  }
 }
 
 // Strategy Interface
 interface PaymentStrategy {
-	void pay(double amount);
+  void pay(double amount);
 }
 
 // Concrete Strategies-1
 class CashPaymentStrategy implements PaymentStrategy {
 
-	@Override
-	public void pay(double amount) {
-		System.out.println("Paid " + amount + " using Cash.");
-	}
+  @Override
+  public void pay(double amount) {
+    System.out.println("Paid " + amount + " using Cash.");
+  }
 }
 
 // Concrete Strategies-2
 class CreditCardPaymentStrategy implements PaymentStrategy {
 
-	@Override
-	public void pay(double amount) {
-		System.out.println("Paid " + amount + " using Credit Card.");
-	}
+  @Override
+  public void pay(double amount) {
+    System.out.println("Paid " + amount + " using Credit Card.");
+  }
 }
 
 // Concrete Strategies-3
 class UpiPaymentStrategy implements PaymentStrategy {
 
-	@Override
-	public void pay(double amount) {
-		System.out.println("Paid " + amount + " using UPI.");
-	}
+  @Override
+  public void pay(double amount) {
+    System.out.println("Paid " + amount + " using UPI.");
+  }
 }
 
 // Context
 class OnlineShoppingCart {
-	private PaymentStrategy paymentStrategy;
+  private PaymentStrategy paymentStrategy;
 
-	// setter method for payment strategy
-	public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
-		this.paymentStrategy = paymentStrategy;
-	}
+  // setter method for payment strategy
+  public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+    this.paymentStrategy = paymentStrategy;
+  }
 
-	public void checkout(double amount) {
-		paymentStrategy.pay(amount);
-	}
+  public void checkout(double amount) {
+    paymentStrategy.pay(amount);
+  }
 }
